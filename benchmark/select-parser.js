ba5a8012c743c3955b23be4beabf33b8fc67ab3b
Nico Kaiser <nico.kaiser@boerse-go.de>
2016-02-10 08:33:58 +0100

    benchmark

---
+++ benchmark/select-parser.js
@@ -0,0 +1,29 @@
+'use strict';
+
+var benchmark = require('benchmark');
+var benchmarks = require('beautify-benchmark');
+
+var parserOld = require('../lib/select-old');
+var parserPeg = require('../lib/select');
+
+var query = 'title,instruments(filter=assetClass.id=1)(order=name:asc)(limit=3)(page=1).quotations(filter=id=4)[quote[value,changePerc]]';
+
+var suite = new benchmark.Suite();
+
+suite.add('select-parser (flora-ql + Buchstabensuppe)', function () {
+    parserOld(query);
+});
+
+suite.add('select-parser (PEG.js)', function () {
+    parserPeg(query);
+});
+
+suite.on('cycle', function (event) {
+    benchmarks.add(event.target);
+});
+
+suite.on('complete', function () {
+    benchmarks.log();
+});
+
+suite.run();
