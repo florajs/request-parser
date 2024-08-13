154167f791c58a2adf4a3551dd14c47285754a46
Nico Kaiser <nico.kaiser@boerse-go.de>
2024-08-13 11:15:00 +0200

    fix: update eslint

---
+++ eslint.config.js
@@ -0,0 +1,20 @@
+const js = require('@eslint/js');
+const eslintPluginPrettierRecommended = require('eslint-plugin-prettier/recommended');
+const globals = require('globals');
+
+module.exports = [
+    {
+        ignores: ['build/*']
+    },
+    js.configs.recommended,
+    eslintPluginPrettierRecommended,
+    {
+        languageOptions: {
+            globals: {
+                ...globals.node
+            },
+            ecmaVersion: 2020,
+            sourceType: 'commonjs'
+        }
+    }
+];
